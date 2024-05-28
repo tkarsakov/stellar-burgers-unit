5a117a08e1b2de1e5af18ef82c3e2715d0f8d7c6
Taras Karsakov <taras.karsakov@intexsoft.by>
2024-05-28 13:51:51 +0300

    Add messages, reformat IngredientTypeTests

--- src/test/java/IngredientTests.java
+++ src/test/java/IngredientTests.java
@@ -21,19 +21,19 @@ public class IngredientTests {
     public void getTypeNoParametersReturnsMockIngredientType() {
         IngredientType mockType = IngredientType.SAUCE;
         Mockito.when(mockIngredient.getType()).thenReturn(mockType);
-        Assert.assertEquals(mockType, mockIngredient.getType());
+        Assert.assertEquals("getType() didn't return correct ingredient type", mockType, mockIngredient.getType());
     }
 
     @Test
     public void getNameNoParametersReturnsMockIngredientName() {
         Mockito.when(mockIngredient.getName()).thenReturn("Mock Ingredient");
-        Assert.assertEquals("Mock Ingredient", mockIngredient.getName());
+        Assert.assertEquals("getName() didn't return correct ingredient name", "Mock Ingredient", mockIngredient.getName());
     }
 
     @Test
     public void getPriceNoParametersReturnsMockIngredientPrice() {
         Mockito.when(mockIngredient.getPrice()).thenReturn(3.5f);
-        Assert.assertEquals(3.5f, mockIngredient.getPrice(), 0.0f);
+        Assert.assertEquals("getPrice() didn't return correct price", 3.5f, mockIngredient.getPrice(), 0.0f);
     }
 
     @Test
@@ -41,20 +41,20 @@ public class IngredientTests {
         IngredientType updatedType = IngredientType.FILLING;
         mockIngredient.type = updatedType;
         Mockito.when(mockIngredient.getType()).thenReturn(updatedType);
-        Assert.assertEquals(updatedType, mockIngredient.getType());
+        Assert.assertEquals("setType() should set correct type", updatedType, mockIngredient.getType());
     }
 
     @Test
     public void setNameUpdatedNameReturnsUpdatedMockIngredientName() {
         mockIngredient.name = "Mockman";
         Mockito.when(mockIngredient.getName()).thenReturn("Mockman");
-        Assert.assertEquals("Mockman", mockIngredient.getName());
+        Assert.assertEquals("setName() should set correct name", "Mockman", mockIngredient.getName());
     }
 
     @Test
     public void setPriceUpdatedPriceReturnsUpdatedMockIngredientPrice() {
         mockIngredient.price = 4.5f;
         Mockito.when(mockIngredient.getPrice()).thenReturn(4.5f);
-        Assert.assertEquals(4.5f, mockIngredient.getPrice(), 0.0f);
+        Assert.assertEquals("setPrice() should set correct price", 4.5f, mockIngredient.getPrice(), 0.0f);
     }
 }
--- src/test/java/IngredientTypeTests.java
+++ src/test/java/IngredientTypeTests.java
@@ -1,21 +1,19 @@
+import org.junit.Assert;
 import org.junit.Test;
 import praktikum.IngredientType;
-
-import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 
 public class IngredientTypeTests {
 
     @Test
     public void valuesNoParametersReturnsAllIngredientTypes() {
         IngredientType[] expectedValues = { IngredientType.SAUCE, IngredientType.FILLING };
-        assertArrayEquals("values() should return all IngredientType constants", expectedValues, IngredientType.values());
+        Assert.assertArrayEquals("values() should return all IngredientType constants", expectedValues, IngredientType.values());
     }
 
     @Test
     public void valueOfValidNameReturnsCorrespondingIngredientType() {
-        assertEquals("valueOf('SAUCE') should return IngredientType.SAUCE", IngredientType.SAUCE, IngredientType.valueOf("SAUCE"));
-        assertEquals("valueOf('FILLING') should return IngredientType.FILLING", IngredientType.FILLING, IngredientType.valueOf("FILLING"));
+        Assert.assertEquals("valueOf('SAUCE') should return IngredientType.SAUCE", IngredientType.SAUCE, IngredientType.valueOf("SAUCE"));
+        Assert.assertEquals("valueOf('FILLING') should return IngredientType.FILLING", IngredientType.FILLING, IngredientType.valueOf("FILLING"));
     }
 
     @Test(expected = IllegalArgumentException.class)
